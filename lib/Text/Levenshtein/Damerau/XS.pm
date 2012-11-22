1436b953a89d0b2e80c2e3ea36a6da61926cc3cc
Nick Logan <ugexe@cpan.org>
2012-11-22 02:07:09 +0000

    Import of UGEXE/Text-Levenshtein-Damerau-XS-1.4 from CPAN.
    gitpan-cpan-distribution: Text-Levenshtein-Damerau-XS
    gitpan-cpan-version:      1.4
    gitpan-cpan-path:         UGEXE/Text-Levenshtein-Damerau-XS-1.4.tar.gz
    gitpan-cpan-author:       UGEXE
    gitpan-cpan-maturity:     released

--- lib/Text/Levenshtein/Damerau/XS.pm
+++ lib/Text/Levenshtein/Damerau/XS.pm
@@ -5,7 +5,7 @@ require Exporter;
 *import = \&Exporter::import;
 require DynaLoader;
 
-$Text::Levenshtein::Damerau::XS::VERSION = '1.3';
+$Text::Levenshtein::Damerau::XS::VERSION = '1.4';
 
 DynaLoader::bootstrap Text::Levenshtein::Damerau::XS $Text::Levenshtein::Damerau::XS::VERSION;
 
